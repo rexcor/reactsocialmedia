6795573da30f8e4a769be5ec722f0cc267d341d3
rexcor <gabrielmdoliveira@gmail.com>
2025-01-11 21:51:25 -0300

    Add userContext to the login portion of the app

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import Header from "./Components/Header.jsx";
 import Footer from "./Components/Footer.jsx";
 import Home from "./Components/Home.jsx";
 import Login from "./Components/Login/Login.jsx";
+import { UserStorage } from "./UserContext.jsx";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
