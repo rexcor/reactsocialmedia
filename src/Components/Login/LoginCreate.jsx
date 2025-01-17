9e03f3987d48af900f1de96a287123dfe5c094bc
rexcor <gabrielmdoliveira@gmail.com>
2025-01-17 19:17:15 -0300

    feat: Implement the login

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Input from "../Form/Input";
+import Button from "../Form/Button.jsx";
+import useForm from "../../Hooks/useForm.jsx";
+import { USER_POST } from "../../api.js";
+import { useContext } from "react";
 
-const LoginPasswordCreate = () => {
-  return <div>Login Criar</div>;
+const LoginCreate = () => {
+  const username = useForm();
+  const email = useForm("email");
+  const password = useForm("password");
+
+  const { userLogin } = React.useContext(useContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+  }
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
 
-export default LoginPasswordCreate;
+export default LoginCreate;
--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: "Preencha um email válido.",
   },
+  password: {
+    regex: /^(?=.*\d)(?=(.*\W){2})(?=.*[a-zA-Z])(?!.*\s).{1,15}$/,
+    message:
+      "A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.",
+  },
 };
 
 const useForm = (type) => {
